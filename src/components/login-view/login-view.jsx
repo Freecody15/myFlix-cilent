91b245fc914e6a17649a9e4c836dc9918973ff3b
Freecody15 <Dakota_pope1996@hotmail.com>
2022-08-30 10:56:28 -0400

    added heroku app to link with my users

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+<<<<<<< Updated upstream
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+=======
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+>>>>>>> Stashed changes
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -8,28 +14,49 @@ export function LoginView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
         /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
+        axios.post('https://dp-movie-api.herokuapp.com/users', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
     };
 
     return (
         <Form>
             <Form.Group controlId="formUsername">
                 <Form.Label>Username:</Form.Label>
+<<<<<<< Updated upstream
                 <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+=======
+                <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+>>>>>>> Stashed changes
             </Form.Group>
             <Button variant="primary" type="submit" onClick={handleSubmit}>
                 Submit
             </Button>
         </Form>
+<<<<<<< Updated upstream
     );
+=======
+    )
+>>>>>>> Stashed changes
 }
 
 LoginView.PropTypes = {
