51369bc58a88f0afac0e829f8a07ec3e8e82644d
Freecody15 <Dakota_pope1996@hotmail.com>
2022-08-30 00:04:55 -0400

    cleaning code up for task

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+import { Button, Card, CardGroup, } from 'react-bootstrap';
 
 import './movie-view.scss';
 
